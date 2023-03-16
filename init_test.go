43d2de9b2f1dc799606c75e6a093aa8838ee105d
olaf michaelis <o.mic@web.de>
2023-03-16 09:02:38 +0100

    Add init tests

---
+++ init_test.go
@@ -0,0 +1,11 @@
+package logseal
+
+import "testing"
+
+func TestInit(t *testing.T) {
+	_ = Init()
+	_ = Init("info")
+	_ = Init("info", "/dev/stdout")
+	_ = Init("info", "/dev/stdout", false)
+	_ = Init("info", "/dev/stdout", false, false)
+}
